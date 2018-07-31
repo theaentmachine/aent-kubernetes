e381bd593eabf93b04ac234be71ac46b326e5941
Jindun SHAO <sjindun@gmail.com>
2018-07-31 12:20:23 +0200

    Update

--- src/aent.php
+++ src/aent.php
@@ -5,9 +5,11 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use TheAentMachine\AentApplication;
 use TheAentMachine\AentKubernetes\Command\NewDeployKubernetesJobEvent;
+use TheAentMachine\Command\CannotHandleAddEventCommand;
 
 $application = new AentApplication();
 
+$application->add(new CannotHandleAddEventCommand());
 $application->add(new NewDeployKubernetesJobEvent());
 
 $application->run();
